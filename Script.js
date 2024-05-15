784693e444e48d982af33252ebe4ff0f07ded601
Sebastian Redruello <misdesarrollos2023@gmail.com>
2024-05-14 23:19:57 -0300

    Correcion de form y responsive

--- Script.js
+++ Script.js
@@ -1,20 +1,20 @@
 function validarFormulario(event) {
-    event.preventDefault(); // Prevenir el envío del formulario por defecto
+    event.preventDefault(); 
 
-    // Obtener los valores de los campos del formulario
+ 
     const nombre = document.getElementById("nombre").value;
     const apellido = document.getElementById("apellido").value;
     const localidad = document.getElementById("localidad").value;
     const email = document.getElementById("email").value;
     const telefono = document.getElementById("telefono").value;
 
-    // Expresiones regulares para validar los campos
+  
     const soloTextoRegex = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/;
     const numerosYTextoRegex = /^[0-9A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/;
     const emailRegex = /\S+@\S+\.\S+/;
     const soloNumerosRegex = /^\d+$/;
 
-    // Validar cada campo
+ 
     if (
         nombre === "" ||
         apellido === "" ||
@@ -34,17 +34,17 @@ function validarFormulario(event) {
     } else if (!soloNumerosRegex.test(telefono)) {
         alert("El campo teléfono solo puede contener números.");
     } else {
-        // Aquí podrías enviar el formulario a través de AJAX si lo deseas
+
         alert("Formulario enviado correctamente.");
     }
 }
 
 function borrarFormulario() {
-    document.getElementById("contactForm").reset();
+    document.getElementById("contactoForm").reset();
 }
 
-// Agregar evento click al botón "Enviar"
+
 document.querySelector("button[type='button'][onclick='validarFormulario()']").addEventListener("click", validarFormulario);
 
-// Agregar evento click al botón "Borrar"
+
 document.querySelector("button[type='button'][onclick='borrarFormulario()']").addEventListener("click", borrarFormulario);
